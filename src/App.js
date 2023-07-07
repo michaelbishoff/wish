0809c0546eb293bbcdc00504467d3a33614cb1cd
Michael Bishoff <michaelsbishoff@gmail.com>
2023-07-07 11:59:48 -0700

    missed one

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from "@firebase/firestore"
 
