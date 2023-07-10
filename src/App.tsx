a07386a6f1c92daa8063a1775c4b9ddf8ba524e2
Michael Bishoff <michaelsbishoff@gmail.com>
2023-07-09 19:54:01 -0700

    Setup a foundation for content

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import './App.css'
+import "./App.css";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from 'firebase/auth';
-import { getFirestore, collection, doc, getDoc, getDocs, setDoc, serverTimestamp, query } from "@firebase/firestore"
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { useEffect, useState } from 'react';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  serverTimestamp,
+  query,
+} from "@firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import Background from "./components/Background";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -26,21 +43,20 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-const auth = getAuth(app)
+const auth = getAuth(app);
 const firestore = getFirestore(app);
 
 export default function App() {
-
-  const [user] = useAuthState(auth)
+  const [user] = useAuthState(auth);
 
   useEffect(() => {
-    const usersRef = collection(firestore, 'users')
+    const usersRef = collection(firestore, "users");
 
-    const unsubscribe = onAuthStateChanged(auth, user => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        console.log("user signed in")
-        const docRef = doc(firestore, "users", user.uid)
-        getDoc(docRef).then(docSnap => {
+        console.log("user signed in");
+        const docRef = doc(firestore, "users", user.uid);
+        getDoc(docRef).then((docSnap) => {
           if (!docSnap.exists()) {
             // no document, so save it
             setDoc(doc(usersRef, user.uid), {
@@ -48,85 +64,88 @@ export default function App() {
               name: user.displayName,
               photoURL: user.photoURL,
               createdAt: serverTimestamp(),
-            })
+            });
           }
-        })
+        });
       } else {
-        console.log("user signed out")
+        console.log("user signed out");
       }
-    })
-    return unsubscribe
-  }, [])
+    });
+    return unsubscribe;
+  }, []);
 
   return (
-    <div className='App'>
-      <header></header>
-      <div className='flex'>
-        Does this auto deploy?
-        {user ? <SignOutAndListUsers user={user} /> : <SignIn />}
-      </div>
+    <div className="App">
+      <Header />
+      <Background content={<MyContent user={user} />} />
     </div>
-  )
+  );
 }
 
 function SignIn() {
   const signInWithGoogle = () => {
-    const provider = new GoogleAuthProvider()
-    signInWithPopup(auth, provider)
-  }
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
   return (
-    <button type="button" className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' onClick={signInWithGoogle}>Sign in with Google</button>
-  )
+    <button
+      type="button"
+      className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      onClick={signInWithGoogle}
+    >
+      Sign in with Google
+    </button>
+  );
 }
 
 function SignOutAndListUsers({ user }: any) {
   return (
-    <div>
+    <div className="mt-8">
       <SignOut user={user} />
       <Users />
     </div>
-  )
+  );
 }
 
 function SignOut({ user }: any) {
-  return user && (
-    <button onClick={() => signOut(auth)}>Sign Out</button>
-  )
+  return user && <button onClick={() => signOut(auth)}>Sign Out</button>;
 }
 
 function Users() {
   // let users: Array<any> = [];
-  const [users, setUsers] = useState<Array<any>>([])
+  const [users, setUsers] = useState<Array<any>>([]);
 
   useEffect(() => {
     async function getUsers() {
-      const q = query(collection(firestore, "users"))
-      const querySnapshot = await getDocs(q)
-      setUsers(querySnapshot.docs.map(doc => {
-        return doc.data()
-      }))
+      const q = query(collection(firestore, "users"));
+      const querySnapshot = await getDocs(q);
+      setUsers(
+        querySnapshot.docs.map((doc) => {
+          return doc.data();
+        })
+      );
       // await getDocs(q).then(querySnapshot => {
       //   users = querySnapshot.docs.map(docSnapShot => {
       //     return docSnapShot.data()
       //   })
       // })
-      console.log("Done setting users")
+      console.log("Done setting users");
     }
-    getUsers()
-  }, [])
+    getUsers();
+  }, []);
 
-  console.log(users.length)
+  console.log(users.length);
   return (
     <ul className="divide-y divide-gray-200">
-      {users.map(user => {
-        return <User key={user.uid} user={user} />
+      {users.map((user) => {
+        return <User key={user.uid} user={user} />;
       })}
     </ul>
-  )
+  );
 }
 
 function User({ user }: any) {
-  console.log("user:", user)
+  console.log("user:", user);
   return (
     <li className="py-4 flex">
       <img className="h-10 w-10 rounded-full" src={user.photoURL} alt="" />
@@ -135,5 +154,28 @@ function User({ user }: any) {
         <p className="text-sm text-gray-500">{user.uid}</p>
       </div>
     </li>
-  )
+  );
+}
+
+function MyContent({ user }: any) {
+  return (
+    <div className="mx-auto max-w-2xl">
+      {user ? <SignOutAndListUsers user={user} /> : <LogInHero />}
+    </div>
+  );
+}
+
+function LogInHero() {
+  return (
+    <div className="py-32 sm:py-48 lg:py-56">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Wish
+        </h1>
+      </div>
+      <div className="mt-20 flex items-center justify-center gap-x-6">
+        <SignIn />
+      </div>
+    </div>
+  );
 }
